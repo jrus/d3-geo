6b47fa607a09fbaf49bcd42d6d628704ffd76bcf
Jacob Rus <me@jacobrus.com>
2020-04-12 13:44:25 -0700

    Switch internal implementation of interpolation from slerp in terms of geographic coordinates to (non-uniform) rational interpolation in stereo coordinates.

--- src/interpolate.js
+++ src/interpolate.js
@@ -1,36 +1,36 @@
-import {asin, atan2, cos, degrees, haversin, radians, sin, sqrt} from "./math.js";
+import {sqrt, hypot} from "./math.js";
+import {planisphere} from "./planisphere.js";
 
-export default function(a, b) {
-  var x0 = a[0] * radians,
-      y0 = a[1] * radians,
-      x1 = b[0] * radians,
-      y1 = b[1] * radians,
-      cy0 = cos(y0),
-      sy0 = sin(y0),
-      cy1 = cos(y1),
-      sy1 = sin(y1),
-      kx0 = cy0 * cos(x0),
-      ky0 = cy0 * sin(x0),
-      kx1 = cy1 * cos(x1),
-      ky1 = cy1 * sin(x1),
-      d = 2 * asin(sqrt(haversin(y1 - y0) + cy0 * cy1 * haversin(x1 - x0))),
-      k = sin(d);
+export default function interpolate(a, b) {
+  const [xa, ya] = planisphere(a);
+  const [xb, yb] = planisphere(b);
 
-  var interpolate = d ? function(t) {
-    var B = sin(t *= d) / k,
-        A = sin(d - t) / k,
-        x = A * kx0 + B * kx1,
-        y = A * ky0 + B * ky1,
-        z = A * sy0 + B * sy1;
-    return [
-      atan2(y, x) * degrees,
-      atan2(z, sqrt(x * x + y * y)) * degrees
-    ];
-  } : function() {
-    return [x0 * degrees, y0 * degrees];
-  };
+  if ((xa === xb) & (ya === yb)) {
+    return function interpolate(t) {
+      return a;
+    }
+  }
 
-  interpolate.distance = d;
-
-  return interpolate;
+  const za = 0.5*(1 - xa*xa - ya*ya), wa = 1 - za;
+  const zb = 0.5*(1 - xb*xb - yb*yb), wb = 1 - zb;
+  const denom = 1 / ((wa*zb + wb*za) +
+    hypot(wa*zb + wb*za, wa*xb + wb*xa, wa*yb + wb*ya));
+  const xm = (wa*xb + wb*xa) * denom;  // midpoint
+  const ym = (wa*yb + wb*ya) * denom;
+  const xu = xm - xa, xv = xb - xm, xh = xb - xa;
+  const yu = ym - ya, yv = yb - ym, yh = yb - ya;
+  const x_uvh = (xu*xv + yu*yv) * xh + (xu*yv - xv*yu) * yh;
+  const y_uvh = (xu*xv + yu*yv) * yh - (xu*yv - xv*yu) * xh;
+  const x_uuh = (xu*xu + yu*yu) * xh;
+  const y_uuh = (xu*xu + yu*yu) * yh;
+  return function interpolate(t) {
+    const t_ = 1 - t;
+    const x_q = xv*t_ + xu*t;
+    const y_q = yv*t_ + yu*t;
+    const q = t / (x_q*x_q + y_q*y_q);  // t / lerp(v,u,t)^2
+    return planisphere.inverse([
+      xa + q * (x_uvh*t_ + x_uuh*t),  // a + q * lerp(uvh,uuh,t)
+      ya + q * (y_uvh*t_ + y_uuh*t)
+    ]);
+  }
 }
