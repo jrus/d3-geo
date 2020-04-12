f85b4ffbdbe514259c3ebb16bf476d9962757e04
Jacob Rus <me@jacobrus.com>
2020-04-12 14:07:56 -0700

    better match d3-geo style

--- src/interpolate.js
+++ src/interpolate.js
@@ -1,33 +1,42 @@
-import {sqrt, hypot} from "./math.js";
+import {hypot} from "./math.js";
 import {planisphere} from "./planisphere.js";
 
 export default function interpolate(a, b) {
-  const [xa, ya] = planisphere(a);
-  const [xb, yb] = planisphere(b);
+  var [xa, ya] = planisphere(a),
+      [xb, yb] = planisphere(b);
 
   if ((xa === xb) & (ya === yb)) {
-    return function interpolate(t) {
-      return a;
-    }
+    return function () { return a; }
   }
+  
+  // TODO: make sure we still pick a meaningful midpoint for a antipodal to b
+  // TODO: properly handle the case where a or b is the pole at infinity
 
-  const za = 0.5*(1 - xa*xa - ya*ya), wa = 1 - za;
-  const zb = 0.5*(1 - xb*xb - yb*yb), wb = 1 - zb;
-  const denom = 1 / ((wa*zb + wb*za) +
-    hypot(wa*zb + wb*za, wa*xb + wb*xa, wa*yb + wb*ya));
-  const xm = (wa*xb + wb*xa) * denom;  // midpoint
-  const ym = (wa*yb + wb*ya) * denom;
-  const xu = xm - xa, xv = xb - xm, xh = xb - xa;
-  const yu = ym - ya, yv = yb - ym, yh = yb - ya;
-  const x_uvh = (xu*xv + yu*yv) * xh + (xu*yv - xv*yu) * yh;
-  const y_uvh = (xu*xv + yu*yv) * yh - (xu*yv - xv*yu) * xh;
-  const x_uuh = (xu*xu + yu*yu) * xh;
-  const y_uuh = (xu*xu + yu*yu) * yh;
+  var za = 0.5*(1 - xa*xa - ya*ya), 
+      zb = 0.5*(1 - xb*xb - yb*yb),
+      wa = 1 - za,
+      wb = 1 - zb,
+      denom = 1 / ((wa*zb + wb*za) +
+        hypot(wa*zb + wb*za, wa*xb + wb*xa, wa*yb + wb*ya)),
+      xm = (wa*xb + wb*xa) * denom,  // midpoint
+      ym = (wa*yb + wb*ya) * denom,
+
+      // See https://observablehq.com/@jrus/circle-arc-interpolation
+      xu = xm - xa,
+      yu = ym - ya,
+      xv = xb - xm,
+      yv = yb - ym,
+      xh = xb - xa,
+      yh = yb - ya,
+      x_uvh = (xu*xv + yu*yv) * xh + (xu*yv - xv*yu) * yh,
+      y_uvh = (xu*xv + yu*yv) * yh - (xu*yv - xv*yu) * xh,
+      x_uuh = (xu*xu + yu*yu) * xh,
+      y_uuh = (xu*xu + yu*yu) * yh;
   return function interpolate(t) {
-    const t_ = 1 - t;
-    const x_q = xv*t_ + xu*t;
-    const y_q = yv*t_ + yu*t;
-    const q = t / (x_q*x_q + y_q*y_q);  // t / lerp(v,u,t)^2
+    var t_ = 1 - t,
+        x_q = xv*t_ + xu*t,
+        y_q = yv*t_ + yu*t,
+        q = t / (x_q*x_q + y_q*y_q);  // t / lerp(v,u,t)^2
     return planisphere.inverse([
       xa + q * (x_uvh*t_ + x_uuh*t),  // a + q * lerp(uvh,uuh,t)
       ya + q * (y_uvh*t_ + y_uuh*t)
--- src/planisphere.js
+++ src/planisphere.js
@@ -1,13 +1,10 @@
-
-const {hypot, atan, atan2, tan, cos, sin} = Math;
-const radians = Math.PI / 180, degrees = 180 / Math.PI;
-
+import {atan, atan2, cos, sin, tan, hypot, radians, degrees} from "./math.js";
 
 export function planisphere([longitude, latitude]) {
   longitude *= radians;
   const stereo_radius = tan(radians * 0.5 * (90 + latitude));
   return [cos(longitude) * stereo_radius, sin(longitude) * stereo_radius];
-};
+}
 
 planisphere.inverse = function planisphereinverse([X, Y]) {
   const longitude = degrees * atan2(Y, X);
