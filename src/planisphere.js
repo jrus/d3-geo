9096587fa482d321ab9dbd1b914dc602209df510
Jacob Rus <me@jacobrus.com>
2020-04-12 13:41:40 -0700

    Add pole-centered stereographic projection for internal use.

---
+++ src/planisphere.js
@@ -0,0 +1,16 @@
+
+const {hypot, atan, atan2, tan, cos, sin} = Math;
+const radians = Math.PI / 180, degrees = 180 / Math.PI;
+
+
+export function planisphere([longitude, latitude]) {
+  longitude *= radians;
+  const stereo_radius = tan(radians * 0.5 * (90 + latitude));
+  return [cos(longitude) * stereo_radius, sin(longitude) * stereo_radius];
+};
+
+planisphere.inverse = function planisphereinverse([X, Y]) {
+  const longitude = degrees * atan2(Y, X);
+  const latitude = 2 * degrees * atan(hypot(X, Y)) - 90;
+  return [longitude, latitude];
+};
