536a58c5c958597704d9f246524d9614e13e8c06
Jacob Rus <me@jacobrus.com>
2020-04-20 02:50:04 -0700

    Test to make sure interpolate lands near the endpoints.

--- src/interpolate.js
+++ src/interpolate.js
@@ -9,10 +9,10 @@ export default function interpolate(a, b) {
   if ((xa === xb) & (ya === yb)) {
     return function () { return a; }
   }
-  
+
   // TODO: make sure we still pick a meaningful midpoint for a antipodal to b
 
-  var za = 0.5*(1 - xa*xa - ya*ya), 
+  var za = 0.5*(1 - xa*xa - ya*ya),
       zb = 0.5*(1 - xb*xb - yb*yb),
       wa = 1 - za,
       wb = 1 - zb,
@@ -24,30 +24,32 @@ export default function interpolate(a, b) {
   // edge case when the midpoint is near infinity
   if (denom*denom === Infinity) {
     return function interpolate(t) {
-      var q = t / (2*t - 1);  // t / lerp(-1,1,t)
+      var q = t / (2*t - 1);
       return planisphere.inverse([
         xa + q * xh,
         ya + q * yh
       ]);
     }
   }
-  
+
   var xm = (wa*xb + wb*xa) * denom,  // midpoint
       ym = (wa*yb + wb*ya) * denom,
-      xu = xm - xa, yu = ym - ya,
-      xv = xb - xm, yv = yb - ym;
-  
+      xu = xm - xa,
+      yu = ym - ya,
+      xv = xb - xm,
+      yv = yb - ym;
+
   // See https://observablehq.com/@jrus/circle-arc-interpolation
-  
+
   // To avoid loss of significance, pick whether to anchor our
   // interpolation at either a or b depending on which one is
   // closer to the origin. This helps protect against the edge
   // case where a is close to the pole at infinity or b is close
   // to the pole at the origin, at the slight computational expense
   // of one extra branch here.
-  var reverse = +(wa > wb),
+  var reverse = +(wa > wb), forward = 1 - reverse,
       x0, y0, x_p0, y_p0, x_p1, y_p1;    
-  if (!reverse) {
+  if (forward) {
     x0 = xa;
     y0 = ya;
     x_p0 = (xu*xv + yu*yv) * xh + (xu*yv - xv*yu) * yh; // uvh
@@ -61,21 +63,21 @@ export default function interpolate(a, b) {
     y_p0 = (xv*xv + yv*yv) * yh;
     x_p1 = (xu*xv + yu*yv) * xh - (xu*yv - xv*yu) * yh; // vuh
     y_p1 = (xu*xv + yu*yv) * yh + (xu*yv - xv*yu) * xh;
-  }
-  
+  }f
+
   // edge case where a or b is infinite
   // We redefine u and v to be [0, 0] and [1, 0] or vice versa
   // as a hack so the function below still behaves as desired.
   if ((xh*xh + yh*yh) === Infinity) {
-    x_p0 = (1 - reverse) * xu;
-    y_p0 = (1 - reverse) * yu;
+    x_p0 = forward * xu;
+    y_p0 = forward * yu;
     x_p1 = reverse * xv;
     y_p1 = reverse * yv;
-    xv = 1 - reverse;
+    xv = forward;
     xu = reverse;
     yv = yu = 0;
   }
-  
+
   return function interpolate(t) {
     var t_ = 1 - t,
         x_q = xv*t_ + xu*t,
--- test/interpolate-test.js
+++ test/interpolate-test.js
@@ -10,15 +10,19 @@ tape("geoInterpolate(a, a) returns a", function(test) {
 
 tape("geoInterpolate(a, b) returns the expected values when a and b lie on the equator", function(test) {
   test.inDelta(d3.geoInterpolate([10, 0], [20, 0])(0.5), [15, 0], 1e-6);
+  test.inDelta(d3.geoInterpolate([10, 0], [20, 0])(0), [10, 0], 1e-6);
+  test.inDelta(d3.geoInterpolate([10, 0], [20, 0])(1), [20, 0], 1e-6);
   test.end();
 });
 
 tape("geoInterpolate(a, b) returns the expected values when a and b lie on a meridian", function(test) {
   test.inDelta(d3.geoInterpolate([10, -20], [10, 40])(0.5), [10, 10], 1e-6);
+  test.inDelta(d3.geoInterpolate([10, -20], [10, 40])(0), [10, -20], 1e-6);
+  test.inDelta(d3.geoInterpolate([10, -20], [10, 40])(1), [10, 40], 1e-6);
   test.end();
 });
 
-tape("geoInterpolate(a, b) passes through the pole a and b are opposite each-other", function(test) {
+tape("geoInterpolate(a, b) passes through the pole when a and b are opposite each-other", function(test) {
   test.inDelta(d3.geoInterpolate([30, -30], [-150, -30])(0.5)[1], -90, 1e-6);
   test.inDelta(d3.geoInterpolate([30, 30], [-150, 30])(0.5)[1], 90, 1e-6);
   test.end();
@@ -26,8 +30,19 @@ tape("geoInterpolate(a, b) passes through the pole a and b are opposite each-oth
 
 tape("geoInterpolate(a, b) works when a or b is at either pole", function(test) {
   test.inDelta(d3.geoInterpolate([60, -90], [30, -50])(0.5), [30, -70], 1e-6);
+  test.inDelta(d3.geoInterpolate([60, -90], [30, -50])(0)[1], -90, 1e-6);  
+  test.inDelta(d3.geoInterpolate([60, -90], [30, -50])(1),[30, -50], 1e-6);
+  
   test.inDelta(d3.geoInterpolate([30, -50], [60, -90])(0.5), [30, -70], 1e-6);
+  test.inDelta(d3.geoInterpolate([30, -50], [60, -90])(0),[30, -50], 1e-6);
+  test.inDelta(d3.geoInterpolate([30, -50], [60, -90])(1)[1], -90, 1e-6);  
+  
   test.inDelta(d3.geoInterpolate([60, 90], [30, 50])(0.5), [30, 70], 1e-6);
+  test.inDelta(d3.geoInterpolate([60, 90], [30, 50])(0)[1], 90, 1e-6);
+  test.inDelta(d3.geoInterpolate([60, 90], [30, 50])(1), [30, 50], 1e-6);
+
   test.inDelta(d3.geoInterpolate([30, 50], [60, 90])(0.5), [30, 70], 1e-6);
+  test.inDelta(d3.geoInterpolate([30, 50], [60, 90])(0), [30, 50], 1e-6);
+  test.inDelta(d3.geoInterpolate([30, 50], [60, 90])(1)[1], 90, 1e-6);
   test.end();
 });
